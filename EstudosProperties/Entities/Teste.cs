dc31039223a6bcd28f064a81b6045c5c051db4b6
CaioAdania <caioadania@gmail.com>
2024-06-10 18:18:06 -0300

    Estudos de Properties

---
+++ EstudosProperties/Entities/Teste.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudosProperties.Entities
+{
+    internal class Teste
+    {
+
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+
+        public Teste(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+
+        }
+
+        public string Nome //propriedade
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value; //O value equivale ao parâmetro que seria o modificador
+                }
+            }
+        }
+
+
+        public double Preco //por não existir o set, não é possivel alterar
+        {
+            get { return _preco; }
+        }
+
+        public int Quantidade //por não existir o set, não é possivel alterar
+        {
+            get { return _quantidade; }
+        }
+    }
+}
---
+++ EstudosProperties/Program.cs
@@ -0,0 +1,17 @@
+﻿using EstudosProperties.Entities;
+
+namespace EstudosProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teste p = new Teste("TV", 500.00, 10);
+
+            p.Nome = "TV4K";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Quantidade);
+        }
+    }
+}
